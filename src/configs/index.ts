699a71b4207ce44b045ae61ca780fd9151b695c8
tiandi <bbextra.tiandi@gmail.com>
2024-03-04 09:24:28 +0800

    Update index.ts
    change def modal

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -17,7 +17,7 @@ export const supportedModels = [
 
 export type SupportedModel = (typeof supportedModels)[number];
 
-export const defaultModel: SupportedModel = 'gpt-3.5-turbo';
+export const defaultModel: SupportedModel = 'gpt-4';
 
 export const supportedImageModels = [
   'DALL-E',
